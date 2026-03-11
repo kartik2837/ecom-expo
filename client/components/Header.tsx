462c0c74cb02d8318da72e0aacf319b8ee902dbd
kartik2837 <ks8783375@gmail.com>
2026-03-10 22:20:31 -0700

    first commit

---
+++ client/components/Header.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <View className='flex-row items-center justify-between px-4 py-3 bg-white'>
+      <Text>Header</Text>
+    </View>
+  )
+}
