abc55ff0d3edfea02863effc6427bfc457e2f939
kartik2837 <ks8783375@gmail.com>
2026-03-18 15:00:37 -0700

    product details ui completed

--- client/context/CartContext.tsx
+++ client/context/CartContext.tsx
@@ -1,5 +1,6 @@
-import { dummyCart, dummyWishlist } from "@/assets/assets";
-import { Product, WishlistContextType } from "@/constants/types";
+
+import { dummyCart } from "@/assets/assets";
+import { Product } from "@/constants/types";
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 export type CartItem = {
     id:string,
